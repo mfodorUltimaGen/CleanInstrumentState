8a40c2db1f15121c39e9194addf4c6f53f3ca49b
mfodor <156849107+mfodorUltimaGen@users.noreply.github.com>
2026-01-29 11:09:47 -0800

    - update file

--- updator.py
+++ updator.py
@@ -1,43 +1,60 @@
 import xml.etree.ElementTree as ET
 import sys
 
-instrumentStateFile = sys.argv[1] + '\\InstrumentState.xml'
+instrument_state_file = sys.argv[1] + '\\InstrumentState.xml'
+# instrument_state_file = 'C:\\Projects\\Pegasus_3.0_QA\\State\\InstrumentState.xml'
+
+
+field_dict = {
+    "ChuckCfg": {
+        "HasWafer": "true",
+        "HasVacuum": "true",
+        "WaferUsed": "true",
+        "CeilingClean": "true"
+    },
+    "Needle": {
+        "InUse": "false",
+    },
+    "BLS": {
+        "HasWafer": "false",
+        "HasVacuum": "false",
+        "WaferUsed": "false",
+    }
+}
 
 
 def update_xml(xml_file):
-    # Parse the XML file
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
+    try:
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
 
-    # Example: Find elements by tag and attribute, and update their values
-    for wafer_elem in root.iter('ChuckCfg'):
-        if 'SampleId' in wafer_elem.attrib:
-            del wafer_elem.attrib['SampleId']
+        # Update the fields from the dict
+        for field_name, field_data in field_dict.items():
+            update_fields(root, field_name, field_data)
 
-        wafer_attributes = ['HasWafer', 'HasVacuum', 'WaferUsed', 'CeilingClean']
-        for wafer_attribute in wafer_attributes:
-            value = wafer_elem.attrib.get(wafer_attribute, '').lower()
-            if value == 'false':
-                wafer_elem.set(wafer_attribute, 'true')
-    for bls_elem in root.iter('BLS'):
-        bls_attributes = ['HasWafer', 'HasVacuum', 'WaferUsed']
-        for bls_attribute in bls_attributes:
-            value = bls_elem.attrib.get(bls_attribute, '').lower()
-            if value == 'true':
-                bls_elem.set(bls_attribute, 'false')
+        # Save the updated XML to a new file or overwrite the existing one
+        updated_xml_file = xml_file.replace('.xml', '.xml')
+        tree.write(updated_xml_file, encoding='utf-8', xml_declaration=True)
 
-    for needle_elem in root.iter('Needle'):
-        needle_in_use = needle_elem.attrib.get('InUse', '').lower()
-
-        if needle_in_use == 'true':
-            needle_elem.set('InUse', 'false')
-
-    # Save the updated XML to a new file or overwrite the existing one
-    updated_xml_file = xml_file.replace('.xml', '.xml')
-    tree.write(updated_xml_file, encoding='utf-8', xml_declaration=True)
-
-    print(f'Updated XML saved to {updated_xml_file}')
+        print(f'Updates saved to {updated_xml_file}')
+    except ET.ParseError:
+        print("Error parsing the XML file. Please check the file format.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
+def update_fields(root, field_name: str, field_data: dict) -> None:
+    # Look for the provided field name
+    for field_elem in root.iter(field_name):
 
-update_xml(instrumentStateFile)
+        # Special case if a Chuck has a SampleID-- should be removed
+        if field_name == 'ChuckCfg':
+            if 'SampleId' in field_elem.attrib:
+                del field_elem.attrib['SampleId']
+
+        # Set the attribute to the proper state
+        for field_attribute, field_state in field_data.items():
+            field_elem.set(field_attribute, field_state)
+
+
+update_xml(instrument_state_file)
